24c966c6cf04cd96fcc39ecd220a6560af8620aa
Dhipu Thazhayamannil Murali <dthazhayaman@cs.stonybrook.edu>
2013-10-12 02:15:46 -0400

    page structure reversed

--- include/sys/memory/page_trans_4kb.h
+++ include/sys/memory/page_trans_4kb.h
@@ -4,17 +4,17 @@
 #include <defs.h>
 
 struct page_trans_4Kb {
-    uint64_t NX:1,
-        available:11,
-        p_PDPE_4Kb:40,
-        AVL:3,
-        bits6_8:3,
-        A:1,
-        PCD:1,
-        PWT:1,
-        US:1,
-        RW:1,
-        P:1;
+    uint64_t P:1,
+	RW:1,
+	US:1,
+	PWT:1,
+	PCD:1,
+	A:1,
+	bits6_8:3,
+	AVL:3,
+	p_PDPE_4Kb:40,
+	available:11, 
+	NX:1;
 };
 
 #endif
