12ba762f05c0b467ff8d50b0b05fba0377d25ded
Dhipu Thazhayamannil Murali <dthazhayaman@cs.stonybrook.edu>
2013-10-12 04:21:41 -0400

    handle_cr2_cr3 implementation

--- include/sys/memory/handle_cr2_cr3.h
+++ include/sys/memory/handle_cr2_cr3.h
@@ -1,14 +1,16 @@
 #ifndef _HANDLE_CR2_CR3_H
 #define _HANDLE_CR2_CR3_H
 
+#include <defs.h>
+
 struct str_cr3 {
-    uint64_t reserved:3,
+    uint64_t reserved_low:3,
         PWT:1,
         PCD:1,
-        reserved:7,
+        reserved_high:7,
         p_PML4E_4Kb:40,
         reserved_MBZ:12;
-}
+};
 
 void* get_cr2();            // Returns virtual address
 struct str_cr3 get_cr3();   // Returns cr3 containing physical address
---
+++ sys/memory/handle_cr2_cr3.c
@@ -0,0 +1,36 @@
+#include <sys/memory/handle_cr2_cr3.h>
+#include <defs.h>
+
+void* get_cr2()
+{
+	uint64_t cr2_value;
+	__asm__(
+		"movq %%cr2, %%rax;"
+		"movq %%rax, %0;"
+		:"=g"(cr2_value)
+		);
+	return (void *)cr2_value;
+}
+
+struct str_cr3 get_cr3()
+{
+        uint64_t cr3_value;
+        __asm__(
+                "movq %%cr3, %%rax;"
+                "movq %%rax, %0;"
+                :"=g"(cr3_value)
+                );
+        return *((struct str_cr3 *)(&cr3_value));
+}
+
+void set_cr3(struct str_cr3 page_trans)
+{
+        uint64_t cr3_value=*((uint64_t *)(&page_trans));
+        __asm__(
+                "movq %%rax, %0;"
+                "movq %%cr3, %%rax;"
+		:
+		:"g"(cr3_value)
+                );
+}
+
