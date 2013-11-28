1c1900c68516f497227a57d27e17a1726f780050
Arun Olappamanna Vasudevan <aolappamanna@cs.stonybrook.edu>
2013-11-27 20:31:46 -0500

    Memory map added for user processes
    Other changes:
    Set user supervisor bit for user malloc()

--- sys/memory/sys_malloc.c
+++ sys/memory/sys_malloc.c
@@ -8,7 +8,8 @@ void* sys_malloc(uint64_t size) {
     struct list_head *vma_list_head = &(getCurrentTask()->mm->mmap);
 
     return (void*) mmap(vma_list_head, MALLOC_START_ADDR, size,
-                                    PAGE_TRANS_READ_WRITE, MAP_ANONYMOUS, 0, 0);
+                            PAGE_TRANS_READ_WRITE | PAGE_TRANS_USER_SUPERVISOR,
+                                                        MAP_ANONYMOUS, 0, 0);
 
 }
 
